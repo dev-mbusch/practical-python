7d3eafc146851aef363324cc496dcaf23f40c1ab
dev-mbusch <dev.mbusch@gmail.com>
2020-06-07 21:00:05 +0200

    Update first session

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,8 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+
+for i in range(1,11):
+    height = round(height * 0.6, 4)
+    print(i, height)
---
+++ Work/log.md
@@ -0,0 +1,3 @@
+2020-06-20:
+- Duration: 60 min
+- Upcoming: 1.5 Lists
