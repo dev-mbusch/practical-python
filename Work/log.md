571fc24dab7dcec0e3ccfd93f7eae0656c5fb80e
dev-mbusch <dev.mbusch@gmail.com>
2020-06-08 19:36:31 +0200

    Update second session

--- Work/log.md
+++ Work/log.md
@@ -1,3 +1,11 @@
-2020-06-20:
+# Work-Log
+
+2020-06-08:
+
+- Duration: 60 min
+- Upcoming: 2.2 Container
+
+2020-06-07:
+
 - Duration: 60 min
 - Upcoming: 1.5 Lists
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,30 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import csv
+import sys
+
+
+def portfolio_cost(filename):
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        print(headers)
+
+        total_cost = 0
+        for row in rows:
+            try:
+                total_cost += int(row[1]) * float(row[2])
+            except ValueError:
+                print('The given file has missing fields!')
+
+    return total_cost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = r'Data/portfolio.csv'
+
+cost = portfolio_cost(r'Data/portfolio.csv')
+print(f'Total Cost: {cost}')
