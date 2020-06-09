d1a5e50b7fbd9ef8519bf232e26260328b11349c
dev-mbusch <dev.mbusch@gmail.com>
2020-06-09 22:23:30 +0200

    Update Session 3

--- Work/log.md
+++ Work/log.md
@@ -1,5 +1,10 @@
 # Work-Log
 
+2020-06-09:
+
+- Duration: 60 min
+- Upcoming: Excercise 2.7
+
 2020-06-08:
 
 - Duration: 60 min
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,49 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+
+def portfolio_cost(filename):
+    """Computes the total cost (shares * cost) of a portfolio file"""
+    total_cost = 0.0
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            nshares = int(row[1])
+            price = float(row[2])
+            total_cost += nshares * price
+    return total_cost
+
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+
+        for row in rows:
+            holding = {
+                'name': row[0],
+                'shares': int(row[1]),
+                'price': float(row[2])
+                }
+            portfolio.append(holding)
+
+    return portfolio
+
+def read_prices(filename):
+    prices = {}
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+    return prices
